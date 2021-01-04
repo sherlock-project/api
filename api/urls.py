5bed9f207df6c567582b0e622437c3290e8ec699
JenChieh <jcs090218@gmail.com>
2021-01-05 01:26:41 +0800

    Add all, WIP.

--- api/urls.py
+++ api/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework import routers
+from cli import views
+
+router = routers.DefaultRouter()
+router.register(r'cli', views.UserViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
--- cli/views.py
+++ cli/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from cli.serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class MyOwnView(APIView):
+    def get(self, request):
+        return Response({'some': 'data'})
