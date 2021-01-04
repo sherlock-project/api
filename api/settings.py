ac6cc0bcfb4ff5a53f95c8893d8b448b7a262360
JenChieh <jcs090218@gmail.com>
2021-01-04 23:47:21 +0800

    Add rest framework settings.

--- api/settings.py
+++ api/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
